3574f69d9863c86e3693149234332485e78ad8cf
Jose Puello <puelloj66@gmail.com>
2024-02-22 15:19:03 -0500

    first commit

---
+++ src/main/java/com/josedev/apirest/Person/Person.java
@@ -0,0 +1,26 @@
+package com.josedev.apirest.Person;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private Integer personID;
+    @Basic
+    private String LastName;
+    private String FirstName;
+    private String Email;
+    private String City; 
+
+
+}
